e803743793352c638d1b271f87af29a480cd4a42
bapinedaf <bapinedaf@hotmail.com>
2020-07-30 11:40:14 -0500

    Cambios a la estructura del proeycto

--- src/test/java/com/example/demo/MicroServicioMySqlRedisApplicationTests.java
+++ src/test/java/com/example/demo/MicroServicioMySqlRedisApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.example.demo.service.CustomerService;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(locations = "classpath:db-test.properties")
+@Sql("/test-mysql.sql")
 class MicroServicioMySqlRedisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+    CustomerService customerService;
+ 
+    @Test
+    public void test() {
+        assertEquals(2, customerService.findAllCustomers().size());
+    }
 
 }
