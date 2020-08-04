e7d4b60195ec7c59323be5d2514a7575e09f3a09
bapinedaf <bapinedaf@hotmail.com>
2020-08-04 15:47:04 -0500

    Fix

--- src/main/java/com/example/demo/configuration/RedisConfiguration.java
+++ src/main/java/com/example/demo/configuration/RedisConfiguration.java
@@ -1,22 +1,30 @@
 package com.example.demo.configuration;
 
 import com.example.demo.entities.Customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfiguration {
+	@SuppressWarnings("deprecation")
 	@Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
+	public RedisConnectionFactory redisConnectionFactory() {
+	        JedisConnectionFactory factory = new JedisConnectionFactory();
+	        factory.setHostName("localhost");
+	        factory.setPort(6379);
+	        return factory;
+	}
 
     @Bean
+    @Autowired
     RedisTemplate<String, Customer> redisTemplate() {
         final RedisTemplate<String, Customer> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
    
