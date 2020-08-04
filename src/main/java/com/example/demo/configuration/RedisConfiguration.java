f99d54f16a37d8404e000b9ffa520105e752d7ae
bapinedaf <bapinedaf@hotmail.com>
2020-08-03 19:11:23 -0500

    Cambio del conyig.yml

--- src/main/java/com/example/demo/configuration/RedisConfiguration.java
+++ src/main/java/com/example/demo/configuration/RedisConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
 public class RedisConfiguration {
@@ -15,10 +14,10 @@ public class RedisConfiguration {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
-        return template;
+    RedisTemplate<String, Customer> redisTemplate() {
+        final RedisTemplate<String, Customer> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
     }
+   
 }
